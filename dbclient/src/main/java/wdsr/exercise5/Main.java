6d56120a605589bc8d39db65e78cb13a77a9a0cd
anik93 <proxianik@gmail.com>
2016-05-25 14:22:23 +0200

    final

--- dbclient/src/main/java/wdsr/exercise5/Main.java
+++ dbclient/src/main/java/wdsr/exercise5/Main.java
@@ -8,7 +8,13 @@ public class Main {
 		DBClient dbClient = new DBClient();
 		dbClient.createTabels();
 		dbClient.insertValues();
+		dbClient.select1();
+		dbClient.select2();
+		dbClient.select3();
+		dbClient.select4();
 		dbClient.select5();
+		dbClient.select6();
+		dbClient.select7();
 		dbClient.closeConnection();
 	}
 
